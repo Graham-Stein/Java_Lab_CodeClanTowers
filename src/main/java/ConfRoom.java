0ae9f8efbf03252e5c4fe4c661c1d43ac563a4e0
Graham-Stein <graham@guavoaviation.co.uk>
2018-11-21 12:12:28 +0000

    Add conference room setup

--- src/main/java/ConfRoom.java
+++ src/main/java/ConfRoom.java
@@ -1,2 +1,40 @@
+import java.util.ArrayList;
+
 public class ConfRoom {
+
+    private String name;
+    private int capacity;
+    private ArrayList<Guest> guests;
+    private boolean audioVisual;
+
+    public ConfRoom(String name, int capacity, boolean audioVisual){
+        this.name = name;
+        this.capacity = capacity;
+        this.audioVisual = audioVisual;
+        this.guests = new ArrayList<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public ArrayList<Guest> getGuests() {
+        return this.guests;
+    }
+
+    public int getNumberOfGuests(){
+        return this.guests.size();
+    }
+
+    public boolean isAudioVisual() {
+        return this.audioVisual;
+    }
+
+    public void addGuest(Guest guest) {
+        this.guests.add(guest);
+    }
 }
--- src/test/java/ConfRoomTest.java
+++ src/test/java/ConfRoomTest.java
@@ -1,2 +1,45 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ConfRoomTest {
+
+    private ConfRoom confRoom;
+    private Guest guest;
+    private Guest guest1;
+
+    @Before
+    public void before(){
+        confRoom = new ConfRoom("Ruby", 2, false);
+        guest = new Guest("Louise");
+        guest1 = new Guest("Nathan");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Ruby", confRoom.getName());
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(2, confRoom.getCapacity());
+    }
+
+    @Test
+    public void hasAudioVisual(){
+        assertEquals(false, confRoom.isAudioVisual());
+    }
+
+    @Test
+    public void hasNoGuests(){
+        assertEquals(0, confRoom.getNumberOfGuests());
+    }
+
+    @Test
+    public void hasGuests(){
+        confRoom.addGuest(guest);
+        confRoom.addGuest(guest1);
+        assertEquals(2, confRoom.getNumberOfGuests());
+    }
 }
